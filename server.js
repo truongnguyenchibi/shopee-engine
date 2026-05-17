f9b9a0ac05199b14a683c77aa4e5f93957a66190
truongnguyenchibi <truongnguyenchibi@gmail.com>
2026-05-17 12:35:32 +0700

    add convert api

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('SERVER RUNNING');
+app.get("/", (req, res) => {
+    res.send("SERVER RUNNING");
 });
 
-app.post('/convert', (req, res) => {
+app.post("/convert", async (req, res) => {
 
     const { url } = req.body;
 
     if (!url) {
         return res.status(400).json({
             success: false,
-            message: 'Missing URL'
+            message: "Missing URL"
         });
     }
 
     return res.json({
         success: true,
         original_url: url,
-        affiliate_url: 'DEMO_LINK'
+        affiliate_url: "DEMO_LINK"
     });
+
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('SERVER RUNNING ON PORT ' + PORT);
+    console.log("SERVER RUNNING ON PORT " + PORT);
 });
